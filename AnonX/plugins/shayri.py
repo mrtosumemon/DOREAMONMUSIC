de52cfbfe9760d938e7539d9d157e92da233ee4d
Gᴏᴅғᴀᴛʜᴇʀ <104144725+mrtosumemon@users.noreply.github.com>
2023-07-24 14:02:48 +0530

    Update shayri.py

--- AnonX/plugins/shayri.py
+++ AnonX/plugins/shayri.py
@@ -83,7 +83,7 @@ async def help(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "💘𝐂ԋαɳɳҽ𝐋💘", url=f"https://t.me/INCRICIBLE_NETWORK"),
+                        "💘𝐂ԋαɳɳҽ𝐋💘", url=f"https://t.me/INCRICIBLE_MUSIC_SUPPORT"),
                      ]
             ]
         ),
