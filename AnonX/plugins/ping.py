0d4d21d69d4018eb4ce12b3bca36671aac0d270d
Gᴏᴅғᴀᴛʜᴇʀ <104144725+mrtosumemon@users.noreply.github.com>
2023-07-24 20:54:52 +0530

    Add files via upload

--- AnonX/plugins/ping.py
+++ AnonX/plugins/ping.py
@@ -2,15 +2,16 @@ from datetime import datetime
 
 from pyrogram import filters
 from pyrogram.types import Message
-
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from config import BANNED_USERS, MUSIC_BOT_NAME, PING_IMG_URL
+import config
 from strings import get_command
-from AnonX import app
-from AnonX.core.call import Anon
-from AnonX.utils import bot_sys_stats
-from AnonX.utils.decorators.language import language
-from AnonX.utils.inline.play import close_keyboard
-
+from Anon import app
+from Anon.core.call import Anon
+from Anon.utils import bot_sys_stats
+from Anon.utils.decorators.language import language
+from Anon.utils.inline.play import close_keyboard
+from Anon.utils.inline.start import BOT_USERNAME
 ### Commands
 PING_COMMAND = get_command("PING_COMMAND")
 
@@ -29,8 +30,69 @@ async def ping_com(client, message: Message, _):
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit_text(
+        _["ping_3"])
+    await response.edit_text(
+        _["ping_4"])
+    await response.edit_text(
         _["ping_2"].format(
             resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK, pytgping
+        ))
+    await response.edit_text(
+       _["ping_5"].format(
+            resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK, pytgping
+        ))
+    await response.edit_text(
+        _["ping_6"].format(
+            resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK, pytgping
+        ))
+    await response.edit_text(
+        _["ping_7"].format(
+            resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK, pytgping
+        ))
+    await response.edit_text(
+        _["ping_8"].format(
+            resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK, pytgping
+        ))
+    await response.edit_text(
+        _["ping_9"].format(
+            resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK, pytgping
+        ))
+    await response.edit_text(
+        _["ping_10"].format(
+            resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK, pytgping
+        ))
+    await response.edit_text(
+        _["ping_11"].format(
+            resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK, pytgping
+        ))
+    await response.edit_text(
+        _["ping_12"].format(
+            resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK, pytgping
         ),
-        reply_markup=close_keyboard
-    )
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+            InlineKeyboardButton(
+                text="✚  𝐀𝐃𝐃 𝐌𝐄 𝐈𝐍 𝐘𝐎𝐔𝐑 𝐆𝐑𝐎𝐔𝐏  ✚",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            )
+        
+        ],
+        [
+            InlineKeyboardButton(
+                text="♦️𝐆𝐑𝐎𝐔𝐏♦️", url=f"https://t.me/Incricible",
+            ),
+            InlineKeyboardButton(
+                text="♦️𝐌𝐎𝐑𝐄♦️", url=f"https://t.me/The_incricible",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="⚙️ 𝐇𝐄𝐋𝐏 ⚙️", callback_data="settings_back_helper"
+            )
+        ],
+    ]
+    ),
+)
+    
+        
